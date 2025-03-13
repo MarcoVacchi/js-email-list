2186277ebf78997e6ecbf531fd66dd8ec6a0b0db
MarcoVacchi <mvacchi816@gmail.com>
2025-03-13 14:27:13 +0100

    feat: complete exercise

--- js/main.js
+++ js/main.js
@@ -16,9 +16,31 @@ Thunder Client (VSCode Extension)*/
 
 
 const endPoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+let emails = ''
+const emailList = document.getElementById("email-list");
+let count = 0;
+//console.log(emailList);
+
+for(let i = 0; i <= 10; i++){
 
  axios.get(endPoint)
  .then(response => {
-    console.log(response.data.response);
- })
 
+    let email = response.data.response;
+    //console.log(email);
+
+    emails += `<li>${email}</li>`;
+
+    count ++;
+
+    if( count === 10){
+      // console.log(emails);
+        emailList.innerHTML = emails;
+    } 
+})
+    .catch(error => {
+        console.error("Errore:", error);
+        //console.log(error);
+    });
+ 
+}
